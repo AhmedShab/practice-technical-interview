332f56381db6c2ab714784ece827bb217f5680f6
Ahmed Shaaban <ahmed.vuw@gmail.com>
2018-02-27 14:09:44 +0300

    finished the matrix exercise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+ let snake = [];
+ for (let i = 0; i < n; i++){
+  snake.push(Array(n));
+ }
+
+ snake.forEach(n => n.fill(0));
+
+ let startRow = 0;
+ let endRow = n - 1;
+ let startcol = 0;
+ let endCol = n - 1;
+ let value = 1;
+
+ while (startcol <= endCol && startRow <= endRow) {
+  for (let i = startcol; i <= endCol; i++) {
+   snake[startRow][i] = value;
+   value++;
+  }
+  startRow++;
+
+  for (let i = startRow; i <= endRow; i++) {
+   snake[i][endCol] = value;
+   value++;
+  }
+  endCol--;
+
+  for (let i = endCol; i >= startcol; i--) {
+   snake[endRow][i] = value;
+   value++;
+  }
+  endRow--;
+
+  for (let i = endRow; i >= startRow; i--) {
+   snake[i][startcol] = value;
+   value++;
+  }
+  startcol++;
+ }
+ return snake;
+}
+
+matrix(3);
 
 module.exports = matrix;
