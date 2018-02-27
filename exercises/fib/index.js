1fc24aeb9ff3a828d792387339e1e5c465154275
Ahmed Shaaban <ahmed.vuw@gmail.com>
2018-02-27 20:57:50 +0300

    finished the fib exercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,58 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+function memoize(fn) {
+ const cache = {};
+ return function (...args) {
+  if (cache[args]) {
+   return cache[args];
+  }
+  else {
+   cache[args] = fn.apply(this, args);
+   return cache[args];
+  }
+ };
+}
+
+const fib = memoize(slowFib);
+
+function slowFib(n) {
+
+ if (n <= 2) {
+  return 1;
+ }
+ else {
+  return fib(n - 2) + fib(n - 1)
+ }
+}
+
+console.log(fib(15));
+
 
 module.exports = fib;
+
+
+// function fib(n, cache = []) {
+//  if (cache[n]) return cache[n];
+//  else if (n <= 2) {
+//   cache[n] = 1;
+//   return cache[n];
+//  }
+//  else {
+//   return cache[n] =  fib(n - 1, cache) + fib(n - 2, cache);
+//  }
+// }
+
+
+// function fib(n) {
+//  let add = [];
+
+//  for (let i = 0; i <= n; i++) {
+//   if (i < 2) add.push(1);
+//   else {
+//    add.push(add[i - 2] + add[i - 1]);
+//   }
+//  }
+//  return add[n - 1];
+// }
