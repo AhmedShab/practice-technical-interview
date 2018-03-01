2532f835536bba5d829607004808d3803d553cb8
Ahmed Shaaban <ahmed.vuw@gmail.com>
2018-03-01 09:40:49 +0300

    finished the stack exercise

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,46 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+ constructor() {
+  this.data = [];
+  this.size = 0;
+ }
+
+ push(value) {
+  this.data[this.size] = value;
+  this.size++;
+ }
+
+ pop() {
+  if (!this.isEmpty()){
+   let value = this.data[this.size - 1];
+   this.size--;
+   this.data.length = this.size;
+   return value;
+  }
+ }
+
+ peek() {
+  if (!this.isEmpty()) {
+   return this.data[this.size - 1];
+  }
+ }
+
+ isEmpty() {
+  if (this.size === 0) return true;
+
+  return false;
+ }
+}
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.pop();
+
+console.log(stack.size);
+
 
 module.exports = Stack;
